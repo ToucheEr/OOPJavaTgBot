d70acbbf2a3af7d6bcef684b42c434a83684894c
ToucheEr <viktor.lebedev@urfu.me>
2021-11-02 01:23:06 +0500

    Load bot project

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# OOPJavaTgBot
+# Javagittgbot
+ 
+# Bot reminder.
+This bot works on the telegram platform can output commands:
+/ help - display information about the bot
+/ get_data - display the calendar for recording reminders
+/ get_time - display the current time
---
+++ src/main/java/Logic.java
@@ -0,0 +1,21 @@
+import javax.xml.crypto.dsig.spec.XSLTTransformParameterSpec;
+import java.util.*;
+
+public class Logic {
+
+
+    public static List<String> inicialization(String str)
+    {
+        ArrayList<String> myArrayList = new ArrayList<String>();
+    return myArrayList;
+    }
+
+
+
+    public static String gettime(){
+        Date j = new Date();
+        String[] g =j.toString().split(" ");
+        String[] f =g[3].split(":");
+        return f[0]+":"+ f[1];
+    }
+}
